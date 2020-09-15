3e19681203ecf4e16b5de4b528fed9dc0b175847
Austin Carter <aucarter@uw.edu>
2020-09-15 13:49:44 -0700

    first stab at pulling in WPP data for CCMPP

--- R/prep_demog.R
+++ R/prep_demog.R
@@ -1,3 +1,27 @@
-## Pull in relevant estimates from WPP 2019 and interpolate
+# Pull in relevant estimates from WPP 2019 and interpolate
 
-library(wpp2019)
+prepWPPdata <- function(datasets = "mx") {
+    data(list = datasets)
+    data_list <- lapply(datasets, function(d) {
+        df <- get(d)
+        df$measure <- d
+        if(typeof(df$age) == "character") {
+            df$age <- as.integer(tstrsplit(df$age, "-")[[1]])
+        }
+        return(df)
+    })
+    dt <- bind_rows(data_list) %>%
+        pivot_longer(
+            contains("-"), 
+            names_to = c("year_start", "year_end"),
+            names_pattern = "(.*)-(.*)",
+            names_transform = list(
+                year_start = as.integer, 
+                year_end = as.integer
+            )
+        )
+    return(dt)
+}
+
+datasets <- c("tfr", "mxF", "mxM", "migration", "sexRatio", "percentASFR")
+dt <- prepWPPdata(datasets)
