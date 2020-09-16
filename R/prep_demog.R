7a1d66d857e5a283e55ccbd6c1e22877278a2b89
Austin Carter <aucarter@uw.edu>
2020-09-16 12:55:30 -0700

    now returns all datasets required for pop_proj

--- R/prep_demog.R
+++ R/prep_demog.R
@@ -1,3 +1,62 @@
-## Pull in relevant estimates from WPP 2019 and interpolate
+# Pull in relevant estimates from WPP 2019 and interpolate
 
-library(wpp2019)
+prepWPPdata <- function(datasets = "mx") {
+    data(list = datasets)
+    data_list <- lapply(datasets, function(d) {
+        df <- get(d)
+        df$measure <- d
+        # Grab the first age if age group used
+        if ("age" %in% names(df)) {
+            if (any(grepl("-", df$age))) {
+                df$age[df$age == "100+"] <- "100"
+                df$age_start <- as.integer(tstrsplit(df$age, "-")[[1]])
+                df$age_end <- as.integer(tstrsplit(df$age, "-")[[2]])
+            } else {
+                df$age_start <- df$age
+                df$age_end <- df$age + 5
+                df$age_end[df$age == 0] <- 1
+                df$age_end[df$age == 1] <- 5
+                df$age_end[df$age == 100] <- NA
+            }
+            df$age <- NULL
+        }
+        # Remove unneeded variables
+        if ("last.observed" %in% names(df)) {
+            df$last.observed <- NULL
+        }
+        # Melt the years long
+        if (any(grepl("-", names(df)))) {
+            # If year interval
+            df <- pivot_longer(
+                df,
+                contains("-"),
+                names_to = c("year_start", "year_end"),
+                names_pattern = "(.*)-(.*)",
+                names_transform = list(
+                    year_start = as.integer,
+                    year_end = as.integer
+                )
+            )
+        } else {
+            # If single year
+            df <- pivot_longer(
+                df,
+                cols = as.character(seq(1950, 2020, 5)),
+                names_to = "year_start",
+                names_transform = list(
+                    year_start = as.integer
+                )
+            )
+        }
+
+        return(df)
+    })
+    dt <- bind_rows(data_list)
+
+    return(dt)
+}
+
+datasets <- c(
+    "tfr", "popM", "popF", "mxF", "mxM", "migration", "sexRatio", "percentASFR"
+)
+dt <- prepWPPdata(datasets)
