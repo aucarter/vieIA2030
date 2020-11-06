5dcbb3e090a43c5570f7721a53f1edbd8a34b398
Austin Carter <aucarter@uw.edu>
2020-11-05 17:36:02 -0800

    use test data for test

--- tests/testthat/test-pop-proj.R
+++ tests/testthat/test-pop-proj.R
@@ -1,6 +1,9 @@
 testthat::test_that("population projection approximately works", {
     # Testing that the difference in the projected deaths is less than 5%
-    proj <- project_pop("South Africa", 2000, 2019)$out_df %>% as.data.table()
+    proj <- project_pop(
+        "South Africa", 2000, 2019, test_data$wpp_input, test_data$obs_wpp
+    )$out_df %>%
+        as.data.table()
     estimate <- proj[group == "CCPM"]$deaths_both
     test <- proj[group == "WPP2019"]$deaths_both
     testthat::expect_true(
