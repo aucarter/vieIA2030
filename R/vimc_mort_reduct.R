6e11b9c955559ed39e87181f06e4c99b40763151
Austin Carter <aucarter@uw.edu>
2020-11-06 13:04:31 -0800

    add function for calculation vimc mortality reduction

---
+++ R/vimc_mort_reduct.R
@@ -0,0 +1,36 @@
+
+#' Calculate all-cause mortality reduction by vaccine for VIMC 10
+#' @param wpp_input Input WPP data
+#' @param obs_wpp Observed WPP data
+#' @param vimc VIMC deaths averted
+#' @return A list of tables with population projection results
+vimc_mort_reduct <- function(wpp_input, obs_wpp, vimc) {
+    # Calculate both-sexes deaths
+    deaths <- get_all_deaths(1999, 2029, wpp_input, obs_wpp) %>%
+        group_by(age, year_id, location_iso3) %>%
+        summarise(deaths_obs = sum(deaths)) %>%
+        ungroup() %>%
+        rename(year = year_id)
+
+    # Merge on VIMC impact estimates and calculate mortality reduction
+    dt <- left_join(vimc, deaths, by = c("age", "year", "location_iso3")) %>%
+        rename(deaths_averted = value) %>%
+        mutate(mort_reduct = 1 - deaths_obs / (deaths_obs + deaths_averted))
+
+    # Check for missingness
+    if (any(is.na(dt$mort_reduct))) {
+        missing_locs <- unique(dt[is.na(mort_reduct)]$location_name)
+        warning(paste("Missing deaths for", paste(missing_locs, collapse = ", ")))
+        dt <- dt[!is.na(mort_reduct)]
+    }
+
+    # Check for non-sensical numbers
+    if (any(range(dt$mort_reduct) < 0 | range(dt$mort_reduct) > 1)) {
+        warning("Over 1 or less than 0 mortality reduction")
+    }
+
+    out_dt <- dt[, .(location_id, age, year, deaths_obs, deaths_averted,
+                     mort_reduct)]
+
+    return(out_dt)
+}
