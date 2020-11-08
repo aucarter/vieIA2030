d3f0e83217d61b9ea6834614c995c23994723499
Austin Carter <aucarter@uw.edu>
2020-11-08 08:53:54 -0800

    switch over to relative risk

--- R/vimc_rr.R
+++ R/vimc_rr.R
@@ -4,7 +4,7 @@
 #' @param obs_wpp Observed WPP data
 #' @param vimc VIMC deaths averted
 #' @return A list of tables with population projection results
-vimc_mort_reduct <- function(wpp_input, obs_wpp, vimc) {
+vimc_rr <- function(wpp_input, obs_wpp, vimc) {
     # Calculate both-sexes deaths
     deaths <- get_all_deaths(1999, 2029, wpp_input, obs_wpp) %>%
         group_by(age, year_id, location_iso3) %>%
@@ -12,25 +12,32 @@ vimc_mort_reduct <- function(wpp_input, obs_wpp, vimc) {
         ungroup() %>%
         rename(year = year_id)
 
+    # Calculate total deaths averted
+    vimc <- vimc %>%
+        group_by(age, year, location_iso3) %>%
+        mutate(deaths_averted = sum(value)) %>%
+        ungroup()
+
     # Merge on VIMC impact estimates and calculate mortality reduction
     dt <- left_join(vimc, deaths, by = c("age", "year", "location_iso3")) %>%
-        rename(deaths_averted = value) %>%
-        mutate(mort_reduct = 1 - deaths_obs / (deaths_obs + deaths_averted))
+        rename(vaccine_deaths_averted = value) %>%
+        mutate(rr = (deaths_obs + deaths_averted - vaccine_deaths_averted) /
+            (deaths_obs + deaths_averted))
 
     # Check for missingness
-    if (any(is.na(dt$mort_reduct))) {
-        missing_locs <- unique(dt[is.na(mort_reduct)]$location_name)
+    if (any(is.na(dt$rr))) {
+        missing_locs <- unique(dt[is.na(rr)]$location_name)
         warning(paste("Missing deaths for", paste(missing_locs, collapse = ", ")))
-        dt <- dt[!is.na(mort_reduct)]
+        dt <- dt[!is.na(rr)]
     }
 
     # Check for non-sensical numbers
-    if (any(range(dt$mort_reduct) < 0 | range(dt$mort_reduct) > 1)) {
+    if (any(range(dt$rr) < 0 | range(dt$rr) > 1)) {
         warning("Over 1 or less than 0 mortality reduction")
     }
 
     out_dt <- dt[, .(location_id, age, year, vaccine_id, deaths_obs,
-                     deaths_averted, mort_reduct)]
+                     deaths_averted, rr)]
 
     return(out_dt)
 }
