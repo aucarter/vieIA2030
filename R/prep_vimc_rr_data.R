bb4f072d12075da720e5e88a7b48a06b50dbc920
Austin Carter <aucarter@uw.edu>
2020-11-09 13:49:59 -0800

    add some messages

--- R/prep_vimc_rr_data.R
+++ R/prep_vimc_rr_data.R
@@ -1,5 +1,6 @@
 prep_vimc_rr_data <- function() {
     ## Pull in data that isn't already loaded in global environment
+    message("Loading data...")
     data_names <- c(
         "wpp_input", "obs_wpp", "vimc_impact_estimates", "coverage_inputs"
     )
@@ -12,11 +13,13 @@ prep_vimc_rr_data <- function() {
     }
 
     # Calculate relative risk
+    message("Calculating relative risk...")
     rr_dt <- vimc_rr(
         wpp_input, obs_wpp, vimc_impact_estimates
     )
 
     # Merge on covariates and coverage
+    message("Merging on covariates and coverage...")
     dt <- left_join(
             rr_dt[year %in% 2000:2019],
             gbd_cov,
