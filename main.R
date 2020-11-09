cffdb8e18a711a8209f091c194104a1b982cffbd
Austin Carter <aucarter@uw.edu>
2020-11-08 21:43:56 -0800

    switch to relative risk

--- main.R
+++ main.R
@@ -1,11 +1,11 @@
 ### Fit a model for VIMC mortality reduction
 
 ## Pull in data
-dt <- prep_vimc_mort_reduct_data()
+dt <- prep_vimc_rr_data()
 
 ## Scatter against covariates
-scatter_mort_reduct <- function(x_var) {
-    gg <- ggplot(dt, aes(x = get(x_var), y = mort_reduct)) +
+scatter_rr <- function(x_var) {
+    gg <- ggplot(dt, aes(x = get(x_var), y = rr)) +
         geom_point(size = 0.1, alpha = 0.2) +
         facet_wrap(~vaccine_short, scales = "free_y") +
         theme_bw() +
@@ -15,19 +15,19 @@ scatter_mort_reduct <- function(x_var) {
 }
 
 library(ggplot2)
-pdf("plots/vimc_mort_reduct_scatters.pdf")
-lapply(c("haqi", "sdi", "coverage"), scatter_mort_reduct)
+pdf("plots/vimc_rr_scatters.pdf")
+lapply(c("haqi", "sdi", "coverage"), scatter_rr)
 dev.off()
 
 ## Simple model
 library(lme4)
-fit_vaccine_mort_reduct <- function(vacc) {
+fit_vaccine_rr <- function(vacc) {
     fit <- lmer(
-        mort_reduct ~ 1 + coverage + sdi + haqi + (1 | location_id),
+        rr ~ 1 + coverage + sdi + haqi + (1 | location_id),
         data = dt[vaccine_short == vacc]
     )
     coefs <- data.table(t(fixef(fit)))
     coefs[, vaccine_short := vacc]
     return(coefs)
 }
-fit_dt <- rbindlist(lapply(unique(dt$vaccine_short), fit_vaccine_mort_reduct))
+fit_dt <- rbindlist(lapply(unique(dt$vaccine_short), fit_vaccine_rr)) 
