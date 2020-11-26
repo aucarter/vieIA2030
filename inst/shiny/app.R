5a37412d3b4e9666b29247641d6694e937d2ff24
Austin Carter <aucarter@uw.edu>
2020-11-25 16:43:46 -0800

    add a shiny app for visualizing vimc estimates by age and year

---
+++ inst/shiny/app.R
@@ -0,0 +1,56 @@
+
+library(shiny)
+
+vimc_impact_estimates <- db_pull("vimc_impact_estimates")
+locs <- sort(unique(vimc_impact_estimates$location_name))
+vaccs <- sort(unique(vimc_impact_estimates$vaccine_long))
+# Define UI for application
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("VIMC Estimates"),
+
+    # Sidebar
+    sidebarLayout(
+        sidebarPanel(
+            width = 2,
+            selectInput("l", "Location:", locs, selected = "Uganda"),
+            selectInput("v", "Vaccine:", vaccs, selected = "Measles"),
+            checkboxInput("log_transform", "Log transform")
+        ),
+
+        # Show a plot
+        mainPanel(
+           plotOutput("plot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$plot <- renderPlot({
+        dt <- vimc_impact_estimates[location_name == input$l & vaccine_long == input$v]
+        cast_dt <- dcast(dt, age ~ year, value.var = "value")
+        cast_dt[, age := NULL]
+        mat <- as.matrix(cast_dt)
+        image(t(mat), xlab = "Year", ylab = "Age")
+        axis(1, at = seq(min(dt$year), max(dt$year), by = 5))
+        
+        gg <- ggplot(dt, aes(x = year, y = age, fill = value)) + 
+            geom_tile() +
+            xlab("Year") + ylab("Age") + labs(fill = "Deaths averted") +
+            scale_fill_viridis(
+                option = "viridis", 
+                direction = -1, 
+                trans = ifelse(input$log_transform, "log10", "identity")) + 
+            theme_minimal() +
+            ggtitle(paste0(input$l, ": ", input$v)) +
+            coord_fixed() +
+            theme(text = element_text(size = 20))
+        print(gg)
+    }, height = 700)
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
