539cb1a3e6fca6dee1ca9b53f881f6c098301fd5
Austin Carter <aucarter@uw.edu>
2020-11-28 14:42:41 -0800

    move gen_db into db_utils

--- R/db_utils.R
+++ R/db_utils.R
@@ -1,9 +1,34 @@
 open_connection <- function() {
-    db_path <- system.file("vieIA2030.db", package = "vieIA2030")
-    my_db <- DBI::dbConnect(RSQLite::SQLite(), db_path)
+    my_db <- DBI::dbConnect(RSQLite::SQLite(), dbname = "vieIA2030.db")
 
     return(my_db)
 }
+
+#' Generates the SQLite database with input data
+gen_db <- function() {
+    if (!file.exists("vieIA2030.db")) {
+        message("No database found -- building now")
+
+        message("   - Prepping vaccine coverage...")
+        source("data-raw/coverage.R")
+
+        message("   - Prepping WPP inputs...")
+        source("data-raw/wpp_input.R")
+
+        message("   - Prepping WPP observed...")
+        source("data-raw/obs_wpp.R")
+
+        if (file.exists("inst/extdata/vimc_estimates.csv")) {
+            message("Prepping VIMC impact estimates...")
+            source("data-raw/vimc_impact.R")
+        } else {
+            warning("Add VIMC estimates to inst/extdata")
+        }
+
+        message("Done!")
+    }
+}
+
 
 list_db_tables <- function() {
     mydb <- open_connection()
@@ -36,3 +61,6 @@ db_pull <- function(table, iso3_list = NULL) {
 
     return(dt)
 }
+
+# Run this during `devtools::load_all()`
+gen_db()
