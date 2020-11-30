93ff4284721d51dd5a4129bd45e93e5a4acfd82e
Austin Carter <aucarter@uw.edu>
2020-11-30 15:57:28 -0800

    udpate gbd data to death couns

--- R/db_utils.R
+++ R/db_utils.R
@@ -19,7 +19,7 @@ gen_db <- function() {
         source("data-raw/obs_wpp.R")
 
         if (file.exists("inst/extdata/vimc_estimates.csv")) {
-            message("Prepping VIMC impact estimates...")
+            message("   - Prepping VIMC impact estimates...")
             source("data-raw/vimc_impact.R")
         } else {
             warning("Add VIMC estimates to inst/extdata")
