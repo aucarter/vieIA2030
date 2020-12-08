6bded7a502db231d3b4b480e5fdccd96d2593781
Austin Carter <aucarter@uw.edu>
2020-12-07 17:55:25 -0800

    add forecast to gbd_covariate prep, extend to 2100 with the complement of exponential decline

--- data-raw/gbd_cov.R
+++ data-raw/gbd_cov.R
@@ -51,4 +51,50 @@ gbd_cov <- merge(
     by = c("location_id", "year")
 )
 
+forecast_gbd_cov <- function(gbd_cov, years_back = 5, plot = F) {
+    fcast_list <- list()
+    max_year <- max(gbd_cov$year)
+    if (plot) {
+        par(mfrow = c(2, 1))
+    }
+    for (cov in c("haqi", "sdi")) {
+        dt <- gbd_cov[, c("location_id", "year", cov), with = F]
+        cast_dt <- dcast(dt, location_id ~ year, value.var = cov)
+        mat <- as.matrix(cast_dt[, -1])
+        start_vec <-  1 - mat[, dim(mat)[2] - years_back]
+        end_vec <-  1 - mat[, dim(mat)[2]]
+        aroc <- log(end_vec / start_vec) / years_back
+        t_mat <- matrix(
+            1:(2100 - max_year), byrow = T, ncol = (2100 - max_year), 
+            nrow = length(end_vec)
+        )
+        pred_mat <- 1 - end_vec * exp(aroc * t_mat)
+        colnames(pred_mat) <- (max_year + 1):2100
+        if (max(pred_mat >= 1)) {
+            warning("Forecast greater than or equal to 1. Consider forecasting 
+                     in logit space")
+        }
+        out_mat <- cbind(mat, pred_mat)
+        if (plot) {
+            matplot(1990:2100, t(out_mat), type = "l", ylim = c(0, 1),
+                    xlab = "Year", ylab = cov)
+            abline(h = 1, col = "red")
+            abline(v = (max_year + 0.5), col = "black")
+        }
+        melt_dt <- melt(
+            data.table(location_id = cast_dt$location_id, out_mat),
+            id.var = "location_id",
+            variable.name = "year",
+            value.name = cov
+        )
+        melt_dt[, year := as.integer(as.character(year))]
+        fcast_list[[cov]] <- melt_dt
+    }
+    out_dt <- merge(fcast_list[[1]], fcast_list[[2]])
+
+    return(out_dt)
+}
+
+gbd_cov <- forecast_gbd_cov(gbd_cov, years_back = 5, plot = T)
+
 usethis::use_data(gbd_cov, overwrite = TRUE)
