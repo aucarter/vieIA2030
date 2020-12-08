412f62ed27e8ce5465bb28e02aa1fa0dfdac8976
Austin Carter <aucarter@uw.edu>
2020-12-07 17:55:55 -0800

    convert gbd to single year in data prep

--- data-raw/gbd_estimates.R
+++ data-raw/gbd_estimates.R
@@ -3,4 +3,22 @@
 ## Load data
 load(system.file("extdata", "gbd19_estimates.RData", package = "vieIA2030"))
 
-usethis::use_data(gbd_estimates, overwrite = TRUE)
+convert_single_year <- function(gbd_dt) {
+    conv_table <- data.table(
+        age = c(0, rep(1, 4), rep(seq(5, 75, 5), each = 5), 80),
+        age_new = 0:80,
+        n = c(1, rep(4, 4), rep(5, 75), 1)
+    )
+    out_dt <- merge(gbd_dt, conv_table, by = "age", allow.cartesian = T)
+    out_dt[, age := age_new]
+    out_dt[, value := value / n]
+    out_dt[, c("age_new", "n") := NULL]
+
+    return(out_dt[])
+}
+
+gbd_estimates <- convert_single_year(gbd_estimates)
+
+mydb <- open_connection()
+DBI::dbWriteTable(mydb, "gbd_vaccine_deaths", gbd_estimates, overwrite = TRUE)
+DBI::dbDisconnect(mydb)
