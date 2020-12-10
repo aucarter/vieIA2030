1d57e6f2b037acef8a697d3b6a2dcd43fc6ee676
Austin Carter <aucarter@uw.edu>
2020-12-10 14:05:45 -0800

    country-specific plots

--- main.R
+++ main.R
@@ -26,29 +26,56 @@ scen_dt <- merge(scen_dt, future_deaths,
     by = c("year", "age", "location_id"), all.x = T)
 scen_dt[!is.na(deaths_obs_future), deaths_obs := deaths_obs_future]
 scen_dt[, averted := get_averted_scen(deaths_obs, coverage, pred_rr, a)]
-
-## Plot total deaths averted by vaccine over time
-vacc_year_dt <- scen_dt[, .(averted = sum(averted, na.rm = T)), by = .(vaccine_short, year)] 
-my_colors <- c(RColorBrewer::brewer.pal(name = "Paired", n = 12), c("darkblue"))
-gg <- ggplot(vacc_year_dt[year %in% 2000:2030], aes(x = year, y = averted / 1e6, fill = vaccine_short)) +
-    geom_area(color = "white", alpha = 0.8) +
-    scale_fill_manual(values = my_colors, name = "Vaccine") +
-    theme_bw() + xlab("Year") + ylab("Deaths averted (in millions)") 
-gg
-
-## Plot with VIMC, non-VIMC, and GBD
 scen_dt[, vimc := ifelse(location_id %in% unique(vimc_impact$location_id), 1, 0)]
 scen_dt[, gbd := ifelse(vaccine_short %in% c("D", "T", "P", "BCG"), 1, 0)]
 scen_dt[vimc == 1 & gbd == 0, label := "VIMC10 (VIMC locations)"]
 scen_dt[vimc == 0 & gbd == 0, label := "Imputed VIMC10 (non-VIMC locations)"]
 scen_dt[gbd == 1, label := "GBD4 (All locations)"]
-label_year_dt <- scen_dt[, .(averted = sum(averted, na.rm = T)), by = .(label, year)] 
-my_colors <- rev(RColorBrewer::brewer.pal(name = "Paired", n = 3))
-gg <- ggplot(label_year_dt[year %in% 2000:2030], aes(x = year, y = averted / 1e6, fill = label)) +
+
+## Plot total deaths averted by vaccine over time
+vacc_year_dt <- scen_dt[, .(averted = sum(averted, na.rm = T)), by = .(vaccine_short, year)] 
+my_colors1 <- c(RColorBrewer::brewer.pal(name = "Paired", n = 12), c("darkblue"))
+my_colors2<- rev(RColorBrewer::brewer.pal(name = "Paired", n = 3))
+## By vaccine
+gg <- ggplot(vacc_year_dt[year %in% 2000:2030], aes(x = year, y = averted / 1e6, fill = vaccine_short)) +
     geom_area(color = "white", alpha = 0.8) +
-    scale_fill_manual(values = my_colors, name = "") +
+    scale_fill_manual(values = my_colors1, name = "Vaccine") +
     theme_bw() + xlab("Year") + ylab("Deaths averted (in millions)") 
 gg
+
+## Plot with VIMC, non-VIMC, and GBD
+label_year_dt <- scen_dt[, .(averted = sum(averted, na.rm = T)), by = .(label, year)] 
+gg <- ggplot(label_year_dt[year %in% 2000:2030], aes(x = year, y = averted / 1e6, fill = label)) +
+    geom_area(color = "white", alpha = 0.8) +
+    scale_fill_manual(values = my_colors2, name = "") +
+    theme_bw() + xlab("Year") + ylab("Deaths averted (in millions)") 
+gg
+
+## Plot all locations
+scen_dt <- merge(scen_dt, loc_table[, .(location_id, location_name)], by = "location_id")
+loc_vacc_year_dt <- scen_dt[, .(averted = sum(averted, na.rm = T)), by = .(location_name, vaccine_short, year)] 
+loc_label_year_dt <- scen_dt[, .(averted = sum(averted, na.rm = T)), by = .(location_name, label, year)] 
+pdf("plots/deaths_averted_by_location.pdf", height = 8, width = 10)
+for(loc in sort(unique(loc_vacc_year_dt$location_name))) {
+    plot_dt1 <- loc_vacc_year_dt[location_name == loc]
+    plot_dt2 <- loc_label_year_dt[location_name == loc]
+    ## By vaccine
+    gg <- ggplot(plot_dt1[year %in% 2000:2030], aes(x = year, y = averted , fill = vaccine_short)) +
+        geom_area(color = "white", alpha = 0.8) +
+        scale_fill_manual(values = my_colors1, name = "Vaccine") +
+        theme_bw() + xlab("Year") + ylab("Deaths averted") +
+        ggtitle(loc)
+    print(gg)
+
+    ## Plot with VIMC, non-VIMC, and GBD
+    gg <- ggplot(plot_dt2[year %in% 2000:2030], aes(x = year, y = averted, fill = label)) +
+        geom_area(color = "white", alpha = 0.8) +
+        scale_fill_manual(values = my_colors2, name = "") +
+        theme_bw() + xlab("Year") + ylab("Deaths averted") +
+        ggtitle(loc)
+    print(gg)
+}
+dev.off()
 
 ## VIMC locations map
 map_locations(unique(vimc_impact$location_iso3), "VIMC Locations")
