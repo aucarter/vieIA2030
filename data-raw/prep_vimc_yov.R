b3069b8df451a3c63f28b7c0274f340b88652223
Austin Carter <aucarter@uw.edu>
2021-01-27 22:24:09 -0800

    take mean deaths_averted and upload to the CLOUD

--- data-raw/prep_vimc_yov.R
+++ data-raw/prep_vimc_yov.R
@@ -1,6 +1,17 @@
 yov_dt <- as.data.table(
     readRDS(("supp_data/interim-update-202007wue_summary.rds"))
 )
+
+# Subset to the correct touchstone
 yov_dt <- yov_dt[touchstone == "201910gavi",
-    .(country, year, vaccine, model, activity_type, coverage, fvps, 
+    .(country, year, vaccine, model, activity_type, coverage, fvps,
     deaths_averted, deaths_averted_rate)]
+
+# Take the mean of the deaths_averted rate across models
+yov_mean_dt <- yov_dt[,
+    lapply(.SD, mean),
+    by = .(country, year, vaccine, activity_type),
+    .SDcols = c("coverage", "fvps", "deaths_averted", "deaths_averted_rate")
+]
+
+upload_object(yov_mean_dt, "vimc_yov_impact")
