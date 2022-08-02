343613ed6000ab3c526496da2c5aeac967a337a4
Austin Carter <aucarter@uw.edu>
2022-08-01 20:51:58 -0700

    remove zeros from vimc data

--- data-raw/vimc_impact.R
+++ data-raw/vimc_impact.R
@@ -6,7 +6,7 @@ setnames(dt, "country", "location_iso3")
 vimc_impact <- dt[order(
     location_iso3, year, age, vaccine
 )]
-# vimc_impact <- vimc_impact[!is.na(deaths_averted) & deaths_averted > 0]
+vimc_impact <- vimc_impact[deaths_averted != 0]
 
 # Convert to strata_id and location_id to save space
 vimc_dt <- merge(vimc_impact, d_v_at_table, by = c("disease", "vaccine", "activity_type"))
