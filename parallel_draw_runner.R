b60bf2971f444bde810778d5b7bd2ff15b79c541
Austin Carter <aucarter@uw.edu>
2022-08-01 20:55:10 -0700

    put draw_idx in the global env for now

--- parallel_draw_runner.R
+++ parallel_draw_runner.R
@@ -1,8 +1,12 @@
 # Organize all draw-level inputs
-vaccine_efficacy <- 
-deaths_averted <- 
-gbd_deaths <- 
-gbd_cov <- 
+vaccine_efficacy <- efficacy_ui
+deaths_averted <- vimc_ui
+gbd_deaths <- gbd_estimates_ui
+gbd_cov <- gbd_cov_ui
+betas <- rbindlist(lapply(list.files("betas", full.names = T), function(f) {
+    id <- as.integer(gsub(".rds", "", strsplit(f, "/")[[1]][2]))
+    b <- as.data.table(readRDS(f))[, d_v_at_id := id]
+}), fill = T, use.names = T)
 
 # Sort them (consider location level)
 
@@ -15,6 +19,7 @@ draw_idx <- data.table::data.table(
 )
 names(draw_idx) <- input_names
 run_num <- 1:n_draws
-draw_idx <- cbind(run_num, draw_idx)
+# TODO: Make this a function rather than putting into global env
+draw_idx <<- cbind(run_num, draw_idx)
 
 #
