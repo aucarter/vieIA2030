168ac08f8a8a50499a1a36fa64c118d6de7734c8
Andrew Shattock <andrewjames.shattock@unibas.ch>
2023-06-13 14:21:41 +0200

    Pulling coverage data from migrated WIISE URLs

--- data-raw/coverage.R
+++ data-raw/coverage.R
@@ -1,153 +1,145 @@
+###########################################################
+# COVERAGE
+#
+# xxxxxxxxxxxx
+#
+###########################################################
+
+# ---------------------------------------------------------
+# xxx
+# Called by: xxx
+# ---------------------------------------------------------
 prep_vimc_coverage_data <- function() {
-    dt <- fread(
-        system.file("extdata", "vimc_coverage.csv", package = "vieIA2030")
-    )
-    vimc_dt <- dt[, lapply(.SD, sum),
-             by = .(country, disease, vaccine, activity_type, year, age, gender),
-             .SDcols = c("fvps_adjusted", "cohort_size")]
-    vimc_dt[, coverage := fvps_adjusted / cohort_size]
-    vimc_dt[, c("disease") := NULL]
-    vimc_dt <- merge(vimc_dt,
-        data.table(gender = c("Both", "Female", "Male"), sex_id = c(3, 2, 1)),
-        by = "gender")
-    vimc_dt[, gender := NULL]
-    setnames(vimc_dt, "fvps_adjusted", "fvps")
-
-    return(vimc_dt[])
+  
+  dt <- fread(
+    system.file("extdata", "vimc_coverage.csv", package = "vieIA2030")
+  )
+  vimc_dt <- dt[, lapply(.SD, sum),
+                by = .(country, disease, vaccine, activity_type, year, age, gender),
+                .SDcols = c("fvps_adjusted", "cohort_size")]
+  vimc_dt[, coverage := fvps_adjusted / cohort_size]
+  vimc_dt[, c("disease") := NULL]
+  vimc_dt <- merge(vimc_dt,
+                   data.table(gender = c("Both", "Female", "Male"), sex_id = c(3, 2, 1)),
+                   by = "gender")
+  vimc_dt[, gender := NULL]
+  setnames(vimc_dt, "fvps_adjusted", "fvps")
+  
+  return(vimc_dt[])
 }
 
+# ---------------------------------------------------------
+# xxx
+# Called by: xxx
+# ---------------------------------------------------------
 prep_wuenic_data <- function() {
-    url <- "http://www.who.int/entity/immunization/monitoring_surveillance/data/coverage_estimates_series.xls"
-    xls <- tempfile()
-    download.file(url, xls, quiet = T, mode = 'wb')
-    sheets <- readxl::excel_sheets(xls)
-    data_list <- lapply(sheets[2:15], function(s) {
-        df <- readxl::read_excel(path = xls, sheet = s)
-        df$Region <- NULL
-        df <- pivot_longer(
-            df,
-            cols = !c("ISO_code", "Cname", "Vaccine"),
-            names_to = "year",
-            names_transform = list(year = as.integer)
-        )
-    })
-    temp <- file.remove(xls)
-
-    dt <- rbindlist(data_list, fill = T)
-    dt[, Cname := NULL]
-    setnames(dt, "ISO_code", "country")
-    setnames(dt, "Vaccine", "wuenic_name")
-    wuenic_dt <- merge(dt, wuenic_vaccine_table, by = "wuenic_name")
-    wuenic_dt[, wuenic_name := NULL]
-
-    wuenic_dt[is.na(value), value := 0]
-    wuenic_dt[, coverage := value / 100]
-    wuenic_dt[, c("age", "activity_type", "sex_id", "value") := .(0, "routine", 3, NULL)]
-
-    return(wuenic_dt[])
+  
+  xls = "coverage_estimates_series.xls"
+  sheets = readxl::excel_sheets(xls)
+  
+  load_sheet_fn = function(sheet) {
+    
+    sheet_dt = readxl::read_excel(path  = xls, 
+                                  sheet = sheet) %>%
+      select(-Region) %>%
+      pivot_longer(cols = !c("ISO_code", "Cname", "Vaccine"),
+                   names_to = "year") %>%
+      mutate(year = as.integer(year))
+    
+    return(sheet_dt)
+  }
+  
+  data_list = lapply(sheets[2:15], load_sheet_fn)
+  
+  data_dt = rbindlist(data_list, fill = T) %>%
+    inner_join(y  = wuenic_vaccine_table, 
+               by = c("Vaccine" = "wuenic_name")) %>%
+    mutate(coverage = ifelse(is.na(value), 0, value / 100), 
+           activity_type = "routine", 
+           sex_id = 3, 
+           age    = 0) %>%
+    select(country = ISO_code, vaccine, activity_type, 
+           sex_id, year, age, coverage)
+  
+  return(data_dt)
 }
 
-
-prep_reported_data <- function() {
-    url <- "http://www.who.int/entity/immunization/monitoring_surveillance/data/coverage_series.xls"
-    xls <- tempfile()
-    download.file(url, xls, quiet = T, mode = 'wb')
-    sheets <- readxl::excel_sheets(xls)
-    dt <- suppressWarnings(
-        data.table(readxl::read_excel(path = xls, sheet = sheets[2]))
-    )
-    temp <- file.remove(xls)
-    dt[, c("WHO_REGION", "Continent", "Asterisc") := NULL]
-    setnames(
-        dt,
-        c("ISO_code", "Vaccine", "Year", "Percent_covrage"),
-        c("country", "vaccine", "year", "value")
-    )
-    dt <- dt[vaccine %in% c("JapEnc", "MenA")]
-    dt[vaccine == "JapEnc", vaccine := "JE"]
-    dt[, sex_id := 3]
-    dt[, coverage := value / 100]
-    dt <- dt[, .(country, sex_id, year, vaccine, coverage)]
-    je_dt <- rbindlist(lapply(0:14, function(a) {
-        copy_dt <- copy(dt[vaccine == "JE"])
-        copy_dt[, age := a]
-    }))
-    mena_dt <- rbindlist(lapply(0:29, function(a) {
-        copy_dt <- copy(dt[vaccine == "MenA"])
-        copy_dt[, age := a]
-    }))
-    dt <- rbind(je_dt, mena_dt)
-    dt[, activity_type := "campaign"]
-
-    return(dt)
+# ---------------------------------------------------------
+# Extract coverage from WIISE for a given vaccine 
+# ---------------------------------------------------------
+extract_coverage = function(v_idx, data_dt, vimc_dt) {
+  
+  # Details of vaccine at index v_idx
+  v = wuenic_vaccine_table[v_idx, ]
+  
+  # Some countries may already be covered for this vaccine by VIMC
+  vimc_country = vimc_dt %>%
+    filter(vaccine == v$vaccine) %>%
+    pull(country) %>%
+    unique()
+  
+  # Filter data by coverage_category type
+  coverage_dt = data_dt %>%
+    setnames(names(.), tolower(names(.))) %>% 
+    rename(country   = code, 
+           wuenic_id = antigen) %>%
+    # Select only vaccine and countries of interest...
+    filter(coverage_category == v$coverage_category, 
+           wuenic_id %in% v$wuenic_id,
+           country  %in% country_table$country, 
+           !country %in% vimc_country) %>%  # Ignoring VIMC countries
+    mutate(vaccine = v$vaccine) %>%
+    # Format coverage...
+    replace_na(list(coverage = 0)) %>%
+    mutate(coverage = coverage/ 100) %>%
+    # Append additional vaccination details...
+    expand_grid(activity_type = v$activity, 
+                sex_id = v$sex, 
+                age    = eval_str(v$age)) %>%
+    select(country, vaccine, activity_type, 
+           sex_id, year, age, coverage) %>%
+    arrange(country, vaccine, year, age) %>%
+    setDT()
+  
+  return(coverage_dt)
 }
 
-prep_hpv_data <- function() {
-    url <- "http://www.who.int/immunization/monitoring_surveillance/data/HPV_estimates.xlsx"
-    xls <- tempfile()
-    download.file(url, xls, quiet = T, mode = 'wb')
-    sheets <- readxl::excel_sheets(xls)
-    dt <- data.table(readxl::read_excel(path = xls, sheet = sheets[2]))
-    temp <- file.remove(xls)
-
-    # NOTE: We are subsetting to only those received a complete dosage!!
-    dt <- dt[grepl("prHPVc", indicator)]
-    dt[, vaccine := "HPV"]
-    setnames(dt, "iso3code", "country")
-    dt[, sex_id := ifelse(sex == "Male", 1, 2)]
-    dt[, value_no_pct := tstrsplit(value_str, "%")[[1]]]
-    dt[value_no_pct == "-", value_no_pct := "0"]
-    dt[, coverage := as.numeric(value_no_pct) / 100]
-    dt <- dt[, .(country, sex_id, year, vaccine, coverage)]
-    # Give the same coverage level to age 8 through 13
-    dt <- rbindlist(lapply(8:13, function(a) {
-        copy_dt <- copy(dt)
-        copy_dt[, age := a]
-    }))
-    dt[, activity_type := "campaign"]
-
-    return(dt)
-}
-
-vimc_dt <- prep_vimc_coverage_data()
-wuenic_dt <- prep_wuenic_data()
-reported_dt <- prep_reported_data()
-hpv_dt <- prep_hpv_data()
-
-non_vimc_dt <- rbindlist(
-    list(wuenic_dt, reported_dt, hpv_dt), use.names = T
-)
-# Sub in WUENIC data for countries not in VIMC
-for (v in unique(non_vimc_dt$vaccine)) {
-    non_vimc_dt <- non_vimc_dt[
-        !(vaccine == v &
-            country %in% unique(vimc_dt[vaccine == v]$country)
-        )
-    ]
-}
-
-# Merge on cohort size and calculate fvps
+# Load population data - used to convert coverage to FVPs
 load_tables("wpp_input")
-both_dt <- wpp_input[, .(cohort_size = sum(nx)), by = .(country, year, age)]
-non_vimc_dt <- merge(non_vimc_dt, country_table[, .(country)])
-non_vimc_dt <- merge(non_vimc_dt, both_dt, by = c("country", "year", "age"))
-non_vimc_dt[, fvps := coverage * cohort_size]
 
-vimc_dt <- merge(vimc_dt, country_table[, .(country, country)])
-coverage <- rbind(vimc_dt, non_vimc_dt)
-coverage[, country := NULL]
+# Load VIMC coverage data
+vimc_dt = prep_vimc_coverage_data()
 
-## Merge on vaccine_id
-coverage <- merge(
-    coverage, v_at_table[, .(vaccine, activity_type, v_at_id)],
-    by = c("vaccine", "activity_type")
-)
-coverage[, c("vaccine", "activity_type") := NULL]
-coverage[, age := as.integer(age)]
-coverage[, sex_id := as.integer(sex_id)]
-coverage <- coverage[order(country, v_at_id, year, age, sex_id),
-                 .(country, v_at_id, year, age, sex_id, fvps, coverage)]
+# Non-VIMC coverage taken from WIISE database
+data_url = "https://whowiise.blob.core.windows.net/upload/coverage--2021.xlsx"
+data_dt  = read_url_xls(data_url, sheet = 1) 
 
-coverage <- coverage[fvps != 0]
+# Extract coverage for each non-VIMC vaccine
+non_vimc_dt = 1 : nrow(wuenic_vaccine_table) %>%
+  lapply(extract_coverage, data_dt, vimc_dt) %>%
+  rbindlist()
 
+# Combine sources
+coverage = wpp_input %>%
+  # Calculate FVPs for non-VIMC diseases...
+  group_by(country, year, age) %>%
+  summarise(cohort_size = sum(nx)) %>%
+  ungroup() %>%
+  inner_join(y  = non_vimc_dt, 
+             by = c("country", "year", "age")) %>%
+  mutate(fvps = coverage * cohort_size) %>%
+  # Combine VIMC diseases and format...
+  bind_rows(vimc_dt) %>%
+  filter(fvps > 0) %>%  # Remove trivial values
+  left_join(y  = v_at_table, 
+            by = c("vaccine", "activity_type")) %>%
+  select(country, v_at_id, year, age, sex_id, fvps, coverage) %>%
+  arrange(country, v_at_id, year, age, sex_id) %>%
+  setDT()
+
+# coverage[, age := as.integer(age)]
+# coverage[, sex_id := as.integer(sex_id)]
+
+# Upload table to database
 upload_object(coverage, "coverage")
+
